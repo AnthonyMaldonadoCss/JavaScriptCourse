b6c5f57d2aef038d9c334e8b3152dc69d00ccaa4
Anthony Maldonado <anthonysistemas20@gmail.com>
2021-04-14 22:29:28 -0400

    more videos: Type conversion and coercion, truthy and falsy values, equality operators, boolean logic (true table)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -159,6 +159,9 @@ console.log(century);
  * Type conversion and coercion
  */
 
+//Type conversion
+
+/*
 const inputYear = '1991';
 
 console.log(Number(inputYear), inputYear);
@@ -166,3 +169,83 @@ console.log(Number(inputYear), inputYear);
 typeof (inputYear);
 
 console.log(inputYear + 18);
+
+console.log(Number("Anthony"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coercion
+
+console.log('I am' + 23 + 'years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+console.log('23' > '18');
+
+let n = '1' + 1; // '11'
+n = n - 1; // '11' = '11' - 1 coercion to number
+
+console.log(n); // 10 number
+*/
+
+/**
+ * 21. Truthy and falsy values
+ */
+
+// 5 falsy values: 0, ' ', undefined, null, NaN
+
+
+/*
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Anthony'));
+console.log(Boolean({}));
+
+const money = 100;
+
+if (money) {
+    console.log("Don't spend it all");
+} else {
+    console.log('You should get a job');
+}
+
+let height;
+
+if (height) {
+    console.log("YAY! height is defined");
+} else {
+    console.log("height is UNDEFINED")
+}
+
+*/
+
+/**
+ * Equality operators: == & ===
+*/
+
+/*
+const age = '18';
+
+if (age === 18) console.log("You just became an adult (Strict)")
+
+if (age == 18) console.log("You just became an adult (loose)")
+
+
+const favourite = Number(prompt("what's your favourite number?"));
+
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+    console.log("Cool! 23 is an amazing number");
+} else if (favourite === 7) {
+    console.log("7 is also a cool number")
+} else {
+    console.log("Number is not 23 or 7")
+}
+
+if (favourite !== 23) {
+    console.log("why not 23?")
+}
+*/
+

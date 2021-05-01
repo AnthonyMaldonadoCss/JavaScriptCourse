6d2f08e8972131271ace833e99d63c690435bdbe
Anthony Maldonado <anthonysistemas20@gmail.com>
2021-04-30 22:07:45 -0400

    Videos and practices

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -13,40 +13,124 @@
 
 // Video2: Functions
 
-function logger() {
-    console.log('My name is Anthony');
-}
+// function logger() {
+//     console.log('My name is Anthony');
+// }
 
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges)
-    const juice = `Juice with ${apples} apples and ${oranges} oranges`
-    return juice
-}
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges)
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`
+//     return juice
+// }
 
-console.log(fruitProcessor(5, 3))
+// console.log(fruitProcessor(5, 3))
 
-const applejuice = fruitProcessor(25, 5);
-console.log(applejuice);
+// const applejuice = fruitProcessor(25, 5);
+// console.log(applejuice);
 
 //Extra
 
-console.log(typeof (0));
+// console.log(typeof (0));
 
-function fruitJuice(x, y) {
-    if (x === 0) {
-        alert(`you have to enter a valid number ${x}`)
-    } else if (y === 0) {
-        alert(`you have to enter a valid number ${y}`)
-    } else {
-        const juiceCall = (`the juice is made up of: ${x} apples y ${y} bananas`)
-        return juiceCall
-    }
+// function fruitJuice(x, y) {
+//     if (x === 0) {
+//         alert(`you have to enter a valid number ${x}`)
+//     } else if (y === 0) {
+//         alert(`you have to enter a valid number ${y}`)
+//     } else {
+//         const juiceCall = (`the juice is made up of: ${x} apples y ${y} bananas`)
+//         return juiceCall
+//     }
+// }
+
+// let x = prompt('enter the number of apples you want in its juice');
+// const clientJuice = fruitJuice(x, 25);
+// console.log(clientJuice);
+
+/**
+ * Video 3
+ * Fuctions declaratios Vs. Expressions
+ */
+
+// fuctions declaratios
+
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1995);
+
+// console.log(age1);
+
+
+//Functions de expression
+
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear
+// }
+
+// const age2 = calcAge2(1995);
+
+// console.log(age1, age2)
+
+// console.log(calcAge2(1995))
+
+/**
+ * Arrow fuctions
+ */
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
 }
 
-let x = prompt('enter the number of apples you want in its juice');
-const clientJuice = fruitJuice(x, 25);
-console.log(clientJuice);
+const calcAge3 = birthYear => 2037 - birthYear;
+
+// the return is implicit
+
+const age3 = calcAge3(1995);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const agefull = 2037;
+    const retirement = 65;
+
+    let mathRetirement = agefull - birthYear;
+    let retirementFull = retirement - mathRetirement
+
+    return `${firstName} retires in ${retirementFull}`
+
+    // return retirementFull;
+}
+
+console.log(yearsUntilRetirement(1995, "Anthony"))
+console.log(yearsUntilRetirement(1997, "Maria"))
+
+const est = a =>
+    a * 3;
+
+console.log(est(2))
+
+
+/**
+ * Functions Calling Other Functions
+*/
+
+
+function cutFruitsPieces(fruits) {
+    return fruits * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitsPieces(apples);
+    const orangePieces = cutFruitsPieces(oranges);
+
+
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`
+    return juice
+}
+
+console.log(fruitProcessor(4, 5))

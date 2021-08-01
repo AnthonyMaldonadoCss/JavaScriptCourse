bbf791120e5585fb7d9aa0c8eace36152f85ea12
Anthony Maldonado <anthonysistemas20@gmail.com>
2021-07-31 23:36:26 -0400

    Destructuring objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -36,19 +36,22 @@ const restaurant = {
       close: 24,
     },
   },
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
 };
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(x);
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(x);
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
 //Switching variables
 // const temp = main;
@@ -56,21 +59,60 @@ console.log(main, secondary);
 // secondary = temp;
 // console.log(main, secondary);
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-console.log(restaurant.order(2, 0));
+// console.log(restaurant.order(2, 0));
 
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
 
 //nested destructuring
-const nested = [2, 3, [4, 5]];
+// const nested = [2, 3, [4, 5]];
 // const [i, , j] = nested;
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
 
 //default values
 // const [p, q, r] = [8, 9];
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+//si no tiene valor que asignar, asigna el 1 que se definio
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, openingHours, tags);
+
+//default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+//si se comienza una linea de codigo, con brackes js da error
+//el espera un bloque de codigo
+//por ende hay que encerrarlo en parantesis
+({ a, b } = obj);
+console.log(a, b);
+
+//nested objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
